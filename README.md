e16da07071bc76dc85750fa70320c75a6bb615f5
Rai Butera <rai@rbutera.com>
2015-01-13 16:45:50 +0000

    finished

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # practice-chat
 Node.js Practice Chat App
+
+## Usage
+1. `npm install`
+2. `node --harmony index.js`
+3. open a browser to [http://localhost:3000](http://localhost:3000)4
--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 #!/usr/bin/env node --harmony
 'use strict';
-const log = require('npmlog'), app = require('express')(), http = require('http').Server(app);
+const log = require('npmlog'),
+      app = require('express')(),
+      http = require('http').Server(app),
+      io = require('socket.io')(http);
 
-log.level('dev');
-
+// route logic
 app.get('/', function(req,res){
   log.info('GET /');
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
+// socket logic
+io.on('connection',function (socket) {
+  log.info('door', 'a user connected');
+
+  socket.broadcast.emit('chat message', 'new user has joined');
+
+  socket.on('chat message', function (msg) {
+    log.info('message', msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function () {
+      io.emit('chat message', 'a user has left');
+      log.info('door', 'a user disconnected');
+  });
+});
+
+
+// boot server
 http.listen(3000,function () {
   log.info('listening on *:3000');
 });
