77434f43ceef050984223d57868d8cb51d453e65
Rai Butera <rai@rbutera.com>
2015-01-13 16:04:41 +0000

    added logger

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node --harmony
 'use strict';
-const app = require('express')(), http = require('http').Server(app);
+const log = require('npmlog'), app = require('express')(), http = require('http').Server(app);
+
+log.level('dev');
+
+app.get('/', function(req,res){
+  log.info('GET /');
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000,function () {
+  log.info('listening on *:3000');
+});
